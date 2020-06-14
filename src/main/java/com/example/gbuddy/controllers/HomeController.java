5b5f5ff99d604e276d3304840820e6c8f1eb10b5
Puneeth <puntirock717@gmail.com>
2020-06-14 17:11:21 +0530

    added a home controller for waking up app on heroku.

---
+++ src/main/java/com/example/gbuddy/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.gbuddy.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @CrossOrigin
+    @GetMapping("/")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("gym buddy server is up and running");
+    }
+
+}
