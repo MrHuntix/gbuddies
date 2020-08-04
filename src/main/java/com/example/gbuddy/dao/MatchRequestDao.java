b0ce7ce26e39c210e84695cb0bb79a854836e5b7
Puneeth <puntirock717@gmail.com>
2020-08-04 12:22:48 +0530

    added dao methods to help in like processor

--- src/main/java/com/example/gbuddy/dao/MatchRequestDao.java
+++ src/main/java/com/example/gbuddy/dao/MatchRequestDao.java
@@ -2,8 +2,12 @@ package com.example.gbuddy.dao;
 
 import com.example.gbuddy.models.entities.MatchRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public interface MatchRequestDao extends JpaRepository<MatchRequest, Integer> {
+    Optional<MatchRequest> getMatchRequest(@Param("lookupRequesterId")int lookupRequesterId, @Param("lookupRequesteeId")int lookupRequesteeId, @Param("userRequesterId")int userRequesterId, @Param("userRequesteeId")int userRequesteeId);
 }
