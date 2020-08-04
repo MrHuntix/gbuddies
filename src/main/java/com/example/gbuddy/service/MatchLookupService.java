1caa43cef4b8c008663f7237f4c3ec13070f2e5e
Puneeth <puntirock717@gmail.com>
2020-08-05 00:01:02 +0530

    updated logic in match lookup service to incorporate buddy requests

--- src/main/java/com/example/gbuddy/service/MatchLookupService.java
+++ src/main/java/com/example/gbuddy/service/MatchLookupService.java
@@ -6,6 +6,7 @@ import com.example.gbuddy.models.constants.CommonConstants;
 import com.example.gbuddy.models.constants.MatchLookupConstants;
 import com.example.gbuddy.models.entities.Match;
 import com.example.gbuddy.models.entities.MatchLookup;
+import com.example.gbuddy.models.entities.MatchRequest;
 import com.example.gbuddy.models.protos.MatchLookupProto;
 import com.example.gbuddy.util.MapperUtil;
 import com.example.gbuddy.models.constants.MatcherConst;
@@ -19,6 +20,9 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
 @Component
 @Transactional
@@ -32,30 +36,36 @@ public class MatchLookupService {
     private MatchDao matchDao;
 
     @Autowired
+    private MatchRequestDao matchRequestDao;
+
+    @Autowired
     private MapperUtil mapperUtil;
 
     @Autowired
     private LikeProcessor likeProcessor;
 
+    private BiPredicate<MatchLookup, MatchLookup> doesRequestExist = (requester, requestee) -> {
+        Optional<MatchRequest> request = matchRequestDao.getMatchRequest(requester.getId(), requestee.getId(), requester.getRequesterId(), requestee.getRequesterId());
+        return !request.isPresent();
+    };
+
     public MatchLookupProto.MatchResponse addForLookup(int requesterId, int gymId, int branchId) {
         MatchLookupProto.MatchResponse.Builder builder = MatchLookupProto.MatchResponse.newBuilder();
         try {
             MatchLookup lookup = matchLookupDao.getRequestMatch(gymId, branchId, requesterId).orElse(null);
-            if (lookup != null && (MatcherConst.MATCHED.getName().equals(lookup.getStatus()) || MatcherConst.UNMATCHED.getName().equals(lookup.getStatus()))) {
+            if (lookup != null) {
                 LOG.info("request already exists(id: {}, status: {})", lookup.getId(), lookup.getStatus());
                 throw new CustomException(String.format(CommonConstants.LOOKUP_REQUEST_EXISTS.getMessage(), lookup.getId(), lookup.getStatus()));
             }
-            if (Objects.isNull(lookup)) {
-                MatchLookup match = new MatchLookup();
-                match.setBranchId(branchId);
-                match.setGymId(gymId);
-                match.setRequesterId(requesterId);
-                match.setStatus(MatcherConst.UNMATCHED.getName());
-                matchLookupDao.save(match);
-                LOG.info("created match(id: {}) for user {}, for gym {}", match.getId(), match.getRequesterId(), match.getGymId());
-                builder.setMessage(CommonConstants.REQUEST_RAISED.getMessage())
-                        .setResponseCode(HttpStatus.OK.value());
-            }
+            MatchLookup match = new MatchLookup();
+            match.setBranchId(branchId);
+            match.setGymId(gymId);
+            match.setRequesterId(requesterId);
+            match.setStatus(MatcherConst.UNMATCHED.getName());
+            matchLookupDao.save(match);
+            LOG.info("created match(id: {}) for user {}, for gym {}", match.getId(), match.getRequesterId(), match.getGymId());
+            builder.setMessage(CommonConstants.REQUEST_RAISED.getMessage())
+                    .setResponseCode(HttpStatus.OK.value());
         } catch (Exception e) {
             builder.setMessage(e.getMessage())
                     .setResponseCode(HttpStatus.UNPROCESSABLE_ENTITY.value());
@@ -145,10 +155,15 @@ public class MatchLookupService {
                 LOG.info("there is no match_lookup entries for requester id :{}", requesterId);
                 throw new CustomException(MatchLookupConstants.NO_MATCH_LOOKUP_RECORD.getMessage());
             }
-            matchLookupsForRequester.forEach(matchLookup -> {
-                List<MatchLookup> matches = matchLookupDao.possibleMatches(matchLookup.getGymId(), matchLookup.getBranchId(), requesterId, matcherConst.getName());
-                if (!CollectionUtils.isEmpty(matches))
-                    mapperUtil.getResponseFromMatchLookup(matches, builder);
+            matchLookupsForRequester.forEach(requester -> {
+                List<MatchLookup> requesteeMatches = matchLookupDao.possibleMatches(requester.getGymId(), requester.getBranchId(), requesterId, matcherConst.getName());
+                if (!CollectionUtils.isEmpty(requesteeMatches)) {
+                    List<MatchLookup> newRequestees = requesteeMatches.stream().filter(requestee -> doesRequestExist.test(requester, requestee)).collect(Collectors.toList());
+                    if(CollectionUtils.isEmpty(newRequestees))
+                        LOG.info("there are no new requestees to derive for matchlookup {}", requester.getId());
+                    else
+                        mapperUtil.getResponseFromMatchLookup(newRequestees, builder);
+                }
             });
             LOG.info("derived {} matches", builder.getLookupsList().size());
             builder.setMessage(MatchLookupConstants.MATCH_LOOKUP_RESPONSE_CREATED.getMessage())
