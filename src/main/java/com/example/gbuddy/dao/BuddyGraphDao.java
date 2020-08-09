502407aad8137f7f6a41ca2903807f2a9819dc4b
Puneeth <puntirock717@gmail.com>
2020-08-09 15:41:12 +0530

    added entity and dao class, updated ddl file to add match request and buddy graph tables

--- src/main/java/com/example/gbuddy/dao/BuddyGraphDao.java
+++ src/main/java/com/example/gbuddy/dao/BuddyGraphDao.java
@@ -4,6 +4,9 @@ import com.example.gbuddy.models.entities.BuddyGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface BuddyGraphDao extends JpaRepository<BuddyGraph, Integer> {
+    List<BuddyGraph> getByUserId(int userId);
 }
