c25d1d820165b6e2f25ec97b7a55c52a812800cf
Puneeth <puntirock717@gmail.com>
2020-10-22 19:53:04 +0530

    updated eureka port and disable spring security

--- gbuddies-api-gateway/src/main/java/com/gbuddies/configuration/WebSecurityConfig.java
+++ gbuddies-api-gateway/src/main/java/com/gbuddies/configuration/WebSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.gbuddies.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.stereotype.Component;
 
 //TODO enable jwt token validation and filter
-//@Component
-//@EnableWebSecurity
+@Component
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${login.url}")
@@ -21,11 +22,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
-
+        http.authorizeRequests().antMatchers("/").permitAll();
         //login and signup requests do not need to be authenticated but other requests need to have jwt token
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, loginUrl, signupUrl).permitAll()
-                .anyRequest().permitAll();
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.POST, loginUrl, signupUrl).permitAll()
+//                .anyRequest().permitAll();
         //to prevent client details to be cached so that we do not authenticate requests without jwt token
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
