d84793ffbcb9d8ff2c396d6d9983cf7e91e0263b
Puneeth <puntirock717@gmail.com>
2020-10-23 00:22:06 +0530

    initial setup for config server

--- gbuddies-config-server/src/main/java/com/gbuddies/ConfigServer.java
+++ gbuddies-config-server/src/main/java/com/gbuddies/ConfigServer.java
@@ -2,10 +2,15 @@ package com.gbuddies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 
+/**
+ * Config server with the config properties at a private git repo
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServer {
 
     @Bean
