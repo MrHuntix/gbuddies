3aa0c9c02a147493e3529caf030d6996269bf796
Puneeth <puntirock717@gmail.com>
2020-10-23 01:36:56 +0530

    added profile for sql

--- gbuddies-common/src/main/java/com/gbuddies/configurations/JpaConfiguration.java
+++ gbuddies-common/src/main/java/com/gbuddies/configurations/JpaConfiguration.java
@@ -3,6 +3,7 @@ package com.gbuddies.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -16,6 +17,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
+@Profile({"sql"})
 public class JpaConfiguration {
 
     @Autowired
